83387fa56b2dd1251ea8eef9e3f2f86cd9062720
AngryDataGirl <117119366+AngryDataGirl@users.noreply.github.com>
2023-05-03 15:21:10 -0400

    Update python_dictionary.py

--- python_dictionary.py
+++ python_dictionary.py
@@ -1,14 +1,25 @@
+# Creating a dictionary 
+
+# Accessing keys
+
+# Accessing values
+
 # Print all key names in the dictionary, one by one:
 
+```
 for x in thisdict:
-print(x)
+  print(x)
+```
 
 # Print all values in the dictionary, one by one:
 
+```
 for x in thisdict:
-print(thisdict[x])
+  print(thisdict[x])
+```
 
 # You can also use the values() function to return values of a dictionary:
-
+```
 for x in thisdict.values():
-print(x)
+  print(x)
+```
