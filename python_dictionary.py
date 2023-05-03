2517ae1861f774986580cd6dc45c14c31fbeee1b
AngryDataGirl <117119366+AngryDataGirl@users.noreply.github.com>
2023-05-03 15:19:35 -0400

    Create python_dictionary.py

---
+++ python_dictionary.py
@@ -0,0 +1,14 @@
+# Print all key names in the dictionary, one by one:
+
+for x in thisdict:
+print(x)
+
+# Print all values in the dictionary, one by one:
+
+for x in thisdict:
+print(thisdict[x])
+
+# You can also use the values() function to return values of a dictionary:
+
+for x in thisdict.values():
+print(x)
